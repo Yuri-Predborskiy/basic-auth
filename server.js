7f90bc1c74620313f1802eda033b701bdef4678f
Yuri Predborski <ext-yurii.predborskyi@here.com>
2019-01-11 08:35:07 +0200

    Data fixes: - move data file into a separate folder - fix data file path - create data file if it doesn't exist when you try to read it. Template fixes: - fix registration page, should show "register" instead of "log in" - fix log in page, should correctly render now Remove express-jwt package. Remove authentication temporarily. Move auth routes into authRouter to simplify path. Add todo for improvements.

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const exjwt = require('express-jwt');
+//const jwt = require('jsonwebtoken');
+
 const userController = require('./users/user.controller');
 
 const app = express();
+// todo: move router into a separate file
 const router = express.Router();
+// todo: move authRouter into a separate file
+const authRouter = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 
 const secret = 'my dirty little secret';
-const jwtMW = exjwt({
-    secret
-});
 
 const logRequests = (req, res, next) => {
     console.info(`${req.method} ${req.originalUrl}`);
@@ -31,20 +31,26 @@ router.get('/', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('register');
 });
+// ideally you want to validate the parameters here, before moving on to controller
 router.post('/register', userController.create);
+
+router.get('/auth', (req, res) => {
+    res.render('auth');
+});
+// ideally you want to validate the parameters here, before moving on to controller
 router.post('/auth', (req, res) => {
-    // authenticate user
+    // todo: authenticate user, if user is correct, redirect to users/id/:id
 });
 
 // further paths require user to be authorized
-router.use(jwtMW);
-router.get('/user/all', userController.getAll);
-router.get('/user/?id', userController.getById);
-router.put('/user/?id', userController.updateById);
-router.delete('/user/?id', userController.deleteById);
+app.use('/users', authRouter);
+router.get('/users/all', userController.getAll);
+router.get('/users/id/:id', userController.getById);
+router.put('/users/id/:id', userController.updateById);
+router.delete('/users/id/:id', userController.deleteById);
 
 // error handler, dead end
-router.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
         res.status(401);
     }
--- users/user.controller.js
+++ users/user.controller.js
@@ -1,8 +1,18 @@
 // in prod: separate controller into database layer (model) and logic controller (and, possibly, request handler)
 const fs = require('fs');
+
+const DATA_FILE_PATH = 'data/data.json';
+
+// todo: replace file read/write operations with mongodb calls
+
+// typically you'd want to send the error down the middleware chain
 function getData() {
+    if (!fs.existsSync(DATA_FILE_PATH)) {
+        // create file if it doesn't exist
+        fs.writeFileSync(DATA_FILE_PATH, '{}');
+    }
     try {
-        let rawData = fs.readFileSync('./data.json');
+        let rawData = fs.readFileSync(DATA_FILE_PATH);
         return JSON.parse(rawData);
     } catch(err) {
         console.error('error reading data from file...', err);
@@ -10,10 +20,11 @@ function getData() {
     return null;
 }
 
+// typically you'd want to send the error down the middleware chain
 function writeData(data) {
     if (!data) return;
     try {
-        fs.writeFileSync('./data.json', JSON.stringify(data));
+        fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(data));
     } catch (err) {
         console.error('error writing new data to file...', err);
     }
@@ -33,29 +44,31 @@ function getAll(req, res) {
     res.send(JSON.stringify(data));
 }
 
+// todo: improvement - generate user ID dynamically (UUID) or use simple numeric index
 function create(req, res) {
     let data = getData();
-    let username = req.body.username;
-    if (data[username]) {
+    let userId = req.body.username;
+    if (data[userId]) {
         res.send('this user already exists!');
     } else {
-        data[username] = {
-            username: username, // username = user id
-            password: req.body.password
+        data[userId] = {
+            username: userId, // username = user id
+            password: req.body.password // never save user password as plain text!
         };
         writeData(data);
         res.send('success');
     }
 }
 
+// todo: add typical checks, like checking if username is not null
 function updateById(req, res) {
     let data = getData();
-    let username = req.body.username;
-    if (!data[username]) {
+    let userId = req.params.id;
+    if (!data[userId]) {
         res.status(404);
     } else {
-        data[username] = {
-            username: username, // username = user id
+        // todo: add more fields to update, currently user name is fixed, only password can be changed
+        data[userId] = {
             password: req.body.password
         };
         writeData(data);
@@ -63,14 +76,20 @@ function updateById(req, res) {
     }
 }
 
+// todo: add error description - what failed, or send error down the middleware string
 function deleteById(req, res) {
     let data = getData();
-    let username = req.body.username;
-    if (!data[username]) {
-        res.status(404);
+    let userId = req.body.username;
+    if (!data[userId]) {
+        res.status(404).send('this user does not exist');
     } else {
-        delete data[username];
-        res.send('success');
+        delete data[userId];
+        if (writeData()) {
+            res.redirect('/auth');
+        } else {
+            console.log('failed to delete user', userId);
+            res.send(`error deleting user ${userId}`);
+        }
     }
 }
 
